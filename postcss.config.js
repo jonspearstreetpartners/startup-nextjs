816bf5cace50be93ddbb7e5d372ecf0012013daf
jonspearstreetpartners <Jon@spearstreetpartners.com>
2025-05-19 17:26:30 -0700

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': true,
+        'custom-properties': true,
+        'custom-media-queries': true,
+      },
+    },
     tailwindcss: {},
     autoprefixer: {},
   },
